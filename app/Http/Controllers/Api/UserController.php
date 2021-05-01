7cd545d99a86d25b962a447ba674550320194e73
Valentin Ursuleac <ursuleacv@gmail.com>
2021-05-01 17:22:20 -0400

    Add API and console

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use CleanArch\Application\UseCases\GetUserInteractor;
+use App\Http\Controllers\Controller;
+use App\Presenters\Http\JsonPresenter;
+use App\UseCaseRequests\GetUserRequest;
+use App\ViewModels\ViewModel;
+
+class UserController extends Controller
+{
+    public function show(GetUserInteractor $useCase, JsonPresenter $presenter, $id)
+    {
+        $response = $useCase->handle(new GetUserRequest((int)$id));
+
+        $viewModel = new ViewModel([
+            'id' => $response->getUserId(),
+            'name' => $response->getUserName(),
+            'email' => $response->getMailAddress()
+        ]);
+        return $presenter->view($viewModel);
+    }
+}
